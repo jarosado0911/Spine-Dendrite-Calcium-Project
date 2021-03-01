289341572d2fd542e8cec8b950d90025e778c46d
James Rosado <jarosado0911@gmail.com>
2021-03-01 18:31:53 -0500

    added example figures to the example codes

---
+++ Example-Runs/Spine5_passiveER_run/testout/makevid.m
@@ -0,0 +1,11 @@
+writerObj = VideoWriter('spine5passive.mp4','MPEG-4');
+open(writerObj);
+for K = 0 : 150
+  filename = sprintf('frame.%0.4d.png', K);
+  thisimage = imread(filename);
+  writeVideo(writerObj, thisimage);
+  filename = sprintf('frame.%0.4d.png', K);
+  thisimage = imread(filename);
+  writeVideo(writerObj, thisimage);
+end
+close(writerObj);
